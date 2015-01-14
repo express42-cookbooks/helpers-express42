45906d8c6bed9303e0396893c8b5be40a2930436
Igor Kurochkin <ikurochkin@express42.com>
2015-01-14 14:01:23 +0300

    Fix mail_to attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,4 +4,4 @@ default['express42']['extra-packages'] = []
 default['express42']['private_networks'] = '192.168.0.0/16,172.16.0.0/12,10.0.0.0/8'
 
 default['express42']['handler']['mail_from'] = 'chef@express42.com'
-default['express42']['handler']['mail_to'] = []
+default['express42']['handler']['mail_to'] = ['']
