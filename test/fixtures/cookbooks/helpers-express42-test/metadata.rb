c9483bdbb1d62e857eefb085a4c9b3560906d463
Igor Kurochkin <ikurochkin@express42.com>
2015-01-14 13:59:42 +0300

    Add fixture cookbook

---
+++ test/fixtures/cookbooks/helpers-express42-test/metadata.rb
@@ -0,0 +1,4 @@
+name 'helpers-express42-test'
+
+depends 'apt'
+depends 'helpers-express42'
---
+++ test/fixtures/cookbooks/helpers-express42-test/recipes/default.rb
@@ -0,0 +1,5 @@
+include_recipe 'apt'
+
+include_recipe 'helpers-express42::mail'
+include_recipe 'helpers-express42::network'
+include_recipe 'helpers-express42::packages'
