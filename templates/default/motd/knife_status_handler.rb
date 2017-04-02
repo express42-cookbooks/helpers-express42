e6432b767e71a63168cf334a15618450ac451dad
Nklya <nklya@hotmail.com>
2017-04-02 19:41:31 +0300

    Fix to use guard clause

--- templates/default/motd/knife_status_handler.rb
+++ templates/default/motd/knife_status_handler.rb
@@ -9,10 +9,9 @@ module Motd
     def initialize; end
 
     def report
-      if run_status.success?
-        File.open("#{Chef::Config[:file_cache_path]}/last_successful_chef_run", 'w') do |file|
-          file.write Time.now.to_i
-        end
+      return unless run_status.success?
+      File.open("#{Chef::Config[:file_cache_path]}/last_successful_chef_run", 'w') do |file|
+        file.write Time.now.to_i
       end
     end
   end
